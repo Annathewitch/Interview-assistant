7ee11df3b97bc527157b3e813a23ac17b2f91fe2
Annathewitch <108777580+Annathewitch@users.noreply.github.com>
2026-04-20 10:14:08 +0800

    Simplify next.config.js by removing unused settings
    Removed TypeScript, ESLint, and images configurations from next.config.js.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
